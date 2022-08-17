ce859ed92622c519ae24f7e8cccec342d3443f9f
everest-dev0323 <tony0323jefferson@gmail.com>
2022-08-17 05:45:24 +0200

    font size

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -41,7 +41,7 @@ const Home = () => {
       <div className="h-full flex flex-col items-center justify-center">
         <Link to="/about-us" className="flex relative bottom-0 flex-col justify-center items-center text-center h-max">
           <img src={'assets/svg/logo.svg'} className="m-auto w-[200px] sm:w-[250px] md:w-[280px] xl:w-[300px]" />
-          <p className="text-[15px] text-[#fff] mt-5 sm:text-[25px]">{text}</p>
+          <p className="text-[15px] text-[#fff] mt-5 sm:text-[22px]">{text}</p>
         </Link>
       </div>
       {bar}
